b8d9e05409dbde735304c60549ec7973182071ce
Blyth He <1414924020@qq.com>
2023-07-15 15:46:38 +0800

    add ntlm_info method

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,12 @@
+import base64
 import logging
 from concurrent.futures import ThreadPoolExecutor, wait
 
+from impacket import ntlm
+from impacket.dcerpc.v5 import transport
+
 from certipy.lib.target import Target
+from impacket.dcerpc.v5.rpcrt import MSRPCHeader
 
 logger = logging.getLogger('utils')
 
@@ -54,12 +59,48 @@ def multi_run(fn, targets, max_worker=10):
         wait(fs)
 
 
+def ntlm_info(target_ip, method='rpc'):
+    available_method = {'smb': f'ncacn_np:{target_ip}[\\PIPE\\netlogon]',
+                        'rpc': f'ncacn_ip_tcp:{target_ip}[135]',
+                        'ldap': f''}
+    target_info = {ntlm.NTLMSSP_AV_DNS_HOSTNAME: '',
+                   ntlm.NTLMSSP_AV_DNS_DOMAINNAME: '',
+                   ntlm.NTLMSSP_AV_DNS_TREENAME: ''}
+
+    bind_data = base64.b64decode('BQALAxAAAABwACAAAQAAALgQuBAAAAAAAQAAAAAAAQAIg6/hH13JEZGkCAArFKD6AwAAAARdiIrr'
+                                 'HMkRn+gIACsQSGACAAAACgIAAAAAAABOVExNU1NQAAEAAAAFAoigAAAAAAAAAAAAAAAAAAAAAA==')
+    if method not in available_method:
+        logger.error(f'available methods: {available_method}')
+    if method != 'ldap':
+        c = transport.DCERPCTransportFactory(available_method.get(method))
+        c.connect()
+        c.send(bind_data)
+        s = c.recv()
+        if s:
+            logger.debug(s)
+            resp = MSRPCHeader(s)
+            auth_data = resp['auth_data']
+            challenge_msg = ntlm.NTLMAuthChallenge(auth_data)
+            av_pairs = ntlm.AV_PAIRS(challenge_msg['TargetInfoFields'])
+            for k in target_info:
+                _, av_data = av_pairs[k]
+                target_info[k] = av_data.decode('utf-16')
+            logger.debug(target_info)
+        else:
+            logger.error(f'no data received')
+
+    else:
+        pass
+
+    return target_info
+
+
 if __name__ == '__main__':
     t = Target.create(dc_ip='192.168.31.110',
                       domain='jd.local',
                       password='1234')
 
-    target_validator(t, ['dc_ip', 'domain', ('hashes', 'password')])
-
+    # target_validator(t, ['dc_ip', 'domain', ('hashes', 'password')])
+    ntlm_info('192.168.31.110', 'smb')
     # dcpis = get_all_dcs(t)
     # print(dcpis)
